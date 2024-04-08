c2bb437422ecf9394c4ad4cf427d7a49067307e6
moulik1111aa <moulik1111a@gmail.com>
2024-04-08 14:39:25 +0530

    Enabled Gemini API

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:57886'],
+    origin: ['http://localhost:3000', 'https://deczfinal.onrender.com/'],
     credentials: true,
     allowedHeaders: ['Access-Control-Allow-Origin']
   }
